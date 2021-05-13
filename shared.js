14e1fe0d16b1f144c98a7995f79155a394b8a114
unknown <matiasariveros.cinco@gmail.com>
2021-05-13 16:49:20 -0300

    adding modal & js

---
+++ shared.js
@@ -0,0 +1,10 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var gettingPlanButtons = document.querySelectorAll('.plan button');
+
+for (let button of gettingPlanButtons) {
+    button.addEventListener('click', function(){
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    })
+}
