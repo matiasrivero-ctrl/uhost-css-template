9f8779f2e69c4eeac8c0578b7cd3f608854f30d6
unknown <matiasariveros.cinco@gmail.com>
2021-05-17 22:44:11 -0300

    nothing changed

--- shared.js
+++ shared.js
@@ -2,6 +2,7 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var gettingPlanButtons = document.querySelectorAll('.plan button');
 
+/* Nothing changed */
 for (let button of gettingPlanButtons) {
     button.addEventListener('click', function(){
         backdrop.style.display = 'block';
