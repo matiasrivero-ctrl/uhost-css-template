5f364b8e5f59f73b222cf99b856cad253ac2efa7
unknown <matiasariveros.cinco@gmail.com>
2021-05-18 16:27:39 -0300

    adding responsive nav and manipulating DOM

--- shared.js
+++ shared.js
@@ -1,11 +1,37 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
 var gettingPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
-/* Nothing changed */
 for (let button of gettingPlanButtons) {
     button.addEventListener('click', function(){
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        /* backdrop.style.display = 'block';
+        modal.style.display = 'block'; */
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     })
 }
+
+backdrop.addEventListener('click', function()  {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if(modalNoButton){
+    modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+    if(modal){         
+        modal.classList.remove('open');
+    }
+
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
